9d24d053840218577d354e8068e5d6b7a012964b
FernandoGarambelM <fgarambel@unsa.edu.pe>
2024-06-28 22:39:01 -0500

    Agregando una lista a views para utilizar el tag for

--- Modelos_Django1/listaContactos/inicio/views.py
+++ Modelos_Django1/listaContactos/inicio/views.py
@@ -5,6 +5,7 @@ def myHomeView(request,*args, **kwargs):
     myContext = {
         'myText' : 'Esto habla sobre nosotros',
         'myNumber' : 123,
+        'myList' : [33, 44, 55],
     }
     return render(request,"home.html", myContext)
 def anotherView(request):
