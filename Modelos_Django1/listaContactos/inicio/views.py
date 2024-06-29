e90e23bd1548a080382c7966649ec39a03623a5b
FernandoGarambelM <fgarambel@unsa.edu.pe>
2024-06-28 22:37:30 -0500

    Creando el Contexto en views.py y agregandolo a los templates

--- Modelos_Django1/listaContactos/inicio/views.py
+++ Modelos_Django1/listaContactos/inicio/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def myHomeView(request,*args, **kwargs):
-    return render(request,"home.html",{})
+    myContext = {
+        'myText' : 'Esto habla sobre nosotros',
+        'myNumber' : 123,
+    }
+    return render(request,"home.html", myContext)
 def anotherView(request):
     return HttpResponse('<h1>Esta es otra vista</h1>')
